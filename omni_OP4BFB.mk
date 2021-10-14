2f0b58cfa1c87952d24b7ac207d54de0fb29e012
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-14 08:10:53 +0200

    OP4BFB: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: oppo Model: CPH2083 Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_OP4BFB.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from OP4BFB device
+$(call inherit-product, device/oppo/OP4BFB/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := OP4BFB
+PRODUCT_NAME := omni_OP4BFB
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := CPH2083
+PRODUCT_MANUFACTURER := oppo
